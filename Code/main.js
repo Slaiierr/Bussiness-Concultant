29f77ee149f6a05f11d249cc699a17470a0f56e4
Slaizer <12811281d@gmail.com>
2023-08-15 00:30:55 +0200

    Rajout de la font + finition navbar + container 1 + container 2 avec images et box

--- Code/main.js
+++
@@ -1,9 +0,0 @@
-const Home = document.querySelector('#Home')
-const Products = document.querySelector('#Products')
-const Service = document.querySelector('#Service')
-const Pricing = document.querySelector('#Pricing')
-const Contacts = document.querySelector('#Contacts')
-
-Home.addEventListener("click", () => {
-  Home.classList.add("selected");
-});
